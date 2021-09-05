fe276dead6b16181287364185b0149f17041ad5a
Maksim Ryzhikov <rv.maksim@gmail.com>
2021-09-05 21:34:36 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# playground [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/maksimr/playground)
---
+++ src/index.js
@@ -0,0 +1,8 @@
+function main() {
+  const appNode = document.getElementById('app');
+  if (appNode) {
+    appNode.innerHTML = 'Hello World!';
+  }
+}
+
+main();
